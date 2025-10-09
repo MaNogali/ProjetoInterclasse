b3bdc969e4ad5f6d52309a0afcfd1092b5d62ba7
MaNogali <maribarosanogali@gmail.com>
2025-10-09 08:44:47 -0300

    Arrumando banco de dados

--- ProjetoInterclasse/obj/Debug/net8.0/ProjetoInterclasse.AssemblyInfo.cs
+++ ProjetoInterclasse/obj/Debug/net8.0/ProjetoInterclasse.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoInterclasse")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f7b926c47de818d89f420b1c7d69202928c9bb0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6ed8a44c0905d9e8213cb160449bf505d5cdf7e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoInterclasse")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoInterclasse")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- dbInterclasse.sql
+++ dbInterclasse.sql
@@ -1,13 +1,19 @@
 create database dbInterclasse;
 use dbInterclasse;
 
-create table tbEstatistica
-(RM smallint,
-idModalidade smallint,
-primary key (RM, idModalidade)
-gol smallint not null,
-cartaoAmarelo smallint not null,
-cartaoVermelho smallint not null);
+create table tbTime
+(pagamento bit not null,
+ano date not null,
+idTime smallint  primary key,
+nomeSala varchar(40) not null,
+camisa varchar(3) not null,
+curso varchar(40) not null,
+periodo varchar(30) not null,
+aptoJogo varchar(3) not null);
+
+create table tbModalidade
+(idModalidade smallint primary key,
+NomeModalidade varchar(50) not null);
 
 create table tbAluno
 (RM smallint primary key,
@@ -20,30 +26,42 @@ curso varchar(50) not null);
 create table tbArbitro
 (podeApitar varchar(3) not null,
 idArbitro smallint not null,
-RM smallint primary key);
+RM smallint primary key,
+constraint fk_Arbitro_RM
+foreign key (RM)
+references tbAluno(RM));
 
+create table tbEstatistica
+(RM smallint,
+idModalidade smallint,
+primary key (RM, idModalidade),
+gol smallint not null,
+cartaoAmarelo smallint not null,
+cartaoVermelho smallint not null,
+constraint fk_Aluno_RM
+foreign key (RM)
+references tbAluno (RM),
 
-create table tbtime
-(pagamento bit not null,
-ano date not null,
-idtime smallint not null,
-nomeSala varchar(40) not null,
-camisa varchar(3) not null,
-curso varchar(40) not null,
-periodo varchar(30) not null,
-aptoJogo varchar(3) not null);
+constraint fk_Modalidade_Id
+foreign key(IdModalidade)
+references tbModalidade(idModalidade));
+
+create table tbPartida
+(idPartida smallint primary key,
+placar varchar(10) not null,
+horarioDia datetime not null,
+descricao varchar(300) not null,
+aulaOcorrida varchar(80) not null);
 
 create table tbChaveamento
 (idPartida smallint not null,
 idChave smallint not null,
-idChave smallint not null,
 fase varchar(50) not null,
 time1 smallint not null,
-time2 smallint not null);
-
-create table modalidade
-(idModalidade smallint primary key,
-NomeModalidade varchar(50) not null);
+time2 smallint not null,
+constraint fk_Chaveamento_IdPartida
+foreign key(idPartida)
+references tbPartida(idPartida));
 
 create table tbGrupo
 (idModalidade smallint primary key,
@@ -51,16 +69,12 @@ time1 smallint not null,
 time2 smallint not null,
 time3 smallint not null,
 time4 smallint not null,
-idGrupo smallint primary key);
+idGrupo smallint primary key,
+constraint fk_Grupo_IdM
+foreign key(idModalidade)
+references tbModalidade(idModalidade));
 
-create table Partida
-(idPartida smallint primary key,
-placar varchar(10) not null,
-horarioDia datetime not null,
-descricao varchar(300) not null,
-aulaOcorrida varchar(80) not null);
-
-create table tbclassicacao
+create table tbClassificacao
 (cartaoAmarelo smallint not null,
 cartaoVermelho smallint not null,
 golsMarcados smallint not null,
@@ -69,6 +83,5 @@ saldoGols smallint not null,
 statusTime varchar(25) not null,
 pontos smallint not null,
 grupo varchar(10) not null,
-idtime samallint not null,
-idmodalidade smallint not null
-)
+idTime smallint not null,
+idModalidade smallint not null);
