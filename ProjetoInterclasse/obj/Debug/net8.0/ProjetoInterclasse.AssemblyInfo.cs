0d21695f0b155e981f160065c4d71e848daccdb8
MaNogali <maribarosanogali@gmail.com>
2025-10-10 10:36:38 -0300

    Ajustando página home e adicionando imagens

--- ProjetoInterclasse/obj/Debug/net8.0/ProjetoInterclasse.AssemblyInfo.cs
+++ ProjetoInterclasse/obj/Debug/net8.0/ProjetoInterclasse.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoInterclasse")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6ed8a44c0905d9e8213cb160449bf505d5cdf7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3bdc969e4ad5f6d52309a0afcfd1092b5d62ba7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoInterclasse")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoInterclasse")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
