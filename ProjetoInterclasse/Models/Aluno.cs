c0fd6af78d6c25e3e6e138e976f41d1e5f092397
therabite <dehmicali5@gmail.com>
2025-10-30 08:07:49 -0300

    Continuando projeto

---
+++ ProjetoInterclasse/Models/Aluno.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoInterclasse.Models
+{
+    public class Aluno
+    {
+        [Display(Name = "RM do aluno")]
+        public int RM { get; set; }
+        [Display(Name = "Nome")]
+        public string? Nome { get; set; }
+        [Display(Name = "Período")]
+        public string? Periodo{ get; set; }
+        [Display(Name = "Idade")]
+        public int Idade { get; set; }
+        [Display(Name = "Numero da camiseta")]
+        public int NumeroCamiseta { get; set; }
+    }
+}
---
+++ ProjetoInterclasse/Models/Time.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoInterclasse.Models
+{
+    public class Time
+    {
+        [Display(Name = "Id do Time")]
+        public int IdTime { get; set; }
+        [Display(Name = "Nome da sala")]
+        public string? NomeSala { get; set; }
+        [Display(Name = "Pagamento")] 
+        public string? Pagamento { get; set; }
+        [Display(Name = "Camisa")]
+        public string? Camisa { get; set; }
+        [Display(Name = "Curso")]
+        public string? Curso { get; set; }
+        [Display(Name = "Periodo")]
+        public string? Periodo { get; set; }
+        [Display(Name = "AptoJogo")]
+        public string? AptoJogo { get; set; }
+    }
+}
