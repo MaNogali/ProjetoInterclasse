38d3ab29e5a29d318ee91a99dac307206902cf35
MaNogali <maribarosanogali@gmail.com>
2025-12-02 21:00:41 -0300

    Atualizando Models do projeto

---
+++ ProjetoInterclasse/Controllers/SistemaController.cs
@@ -0,0 +1,141 @@
+﻿
+using System.Linq;
+using System.Web.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using ProjetoInterclasse.Models;
+using ProjetoInterclasse.Data;
+using ProjetoInterclasse.Models;
+
+namespace ProjetoInterclasse.Controllers
+{
+    public class SistemaController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // MENU PRINCIPAL
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // ------------------ ALUNO ------------------
+        public ActionResult CadastroAluno()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CadastroAluno(Aluno aluno)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Alunos.Add(aluno);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(aluno);
+        }
+
+        public ActionResult ListaAlunos()
+        {
+            var alunos = db.Alunos.ToList();
+            return View(alunos);
+        }
+
+        // ------------------ TIME ------------------
+        public ActionResult CadastroTime()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CadastroTime(Time time)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Times.Add(time);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(time);
+        }
+
+        public ActionResult ListaTimes()
+        {
+            var times = db.Times.ToList();
+            return View(times);
+        }
+
+        // ------------------ PARTIDA ------------------
+        public ActionResult CadastroPartida()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CadastroPartida(Partida partida)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Partidas.Add(partida);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(partida);
+        }
+
+        // ------------------ GRUPO ------------------
+        public ActionResult CadastroGrupo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CadastroGrupo(Grupo grupo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Grupos.Add(grupo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(grupo);
+        }
+
+        // ------------------ CHAVEAMENTO ------------------
+        public ActionResult CadastroChaveamento()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CadastroChaveamento(Chaveamento chave)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Chaveamentos.Add(chave);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(chave);
+        }
+
+        // ------------------ CLASSIFICACAO ------------------
+        public ActionResult CadastroClassificacao()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CadastroClassificacao(Classificacao classificacao)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Classificacoes.Add(classificacao);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(classificacao);
+        }
+    }
+}
---
+++ ProjetoInterclasse/Models/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace ProjetoInterclasse.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+            base ("Server=localhost;database=dbInterclasse;user=root;password=12345678")
+          {
+            }
+        public DbSet<Aluno> Aluno { get; set; }
+        public DbSet<Time> Time { get; set; }
+        public DbSet<Grupo> Grupo { get; set; }
+        public DbSet<Chaveamento> Chaveamento { get; set; }
+        public DbSet<Classificacao> Classificassao { get; set; }
+        public DbSet<Partida> Partida { get; set; }
+    }
+}
